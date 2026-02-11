4709e7fc1031ddfccdccef899cdc5b431063715d
joaobatistamedeiroscf <joaobatistamedeiroscf@gmail.com>
2026-02-11 14:48:24 -0300

    import fruits directly from db.json instead of fetching from localhost

--- src/components/FruitsCard.tsx
+++ src/components/FruitsCard.tsx
@@ -1,42 +1,63 @@
-import { useEffect,useState } from "react";
-import style from "./FruitsCard.module.css"
+import { useEffect, useState } from "react";
+import style from "./FruitsCard.module.css";
+import db from "../data/db.json";
+
+interface Fruit {
+  id: string;
+  name: string;
+  price: number;
+  description: string;
+  image: string;
+}
 
 function FruitsCard() {
-  const [fruits , setFruits] = useState([]); 
+  const [fruits, setFruits] = useState<Fruit[]>([]);
 
   useEffect(() => {
-    fetch("http://localhost:3000/fruits")
-    .then((response) => response.json())
-    .then((data) => setFruits(data))
-    .catch((error) => console.error("Erro ao buscar frutas:", error));
+    setFruits(db.fruits);
   }, []);
 
   return (
     <div className={style.container}>
       <div className={style.header}>
-         <h2 className = {style.title}>Frutas Cadastradas: </h2>
+        <h2 className={style.title}>Frutas Cadastradas: </h2>
       </div>
-        <div className={style.fruitContainer}>
-          <ul className={style.fruitList}>
-              {fruits.map((fruit: { id: string, name: string; price: number , description: string , image:string}) => (
-                <li className={style.fruitItem} key={fruit.id}>
-                 {fruit.image && <img className={style.fruitImage} src={fruit.image} alt={fruit.name} />}
-                  <div className={style.fruitInfo}>
-                     {fruit.name}  
-                      <br />
-                      Preço: R${fruit.price.toFixed(2)}
-                      {fruit.description && <p className={style.fruitDescription}>{fruit.description}</p>}
-                      <div className={style.buttonContainer}>
-                        <button className={style.addButton}>Comprar</button>
-                      </div>
+      <div className={style.fruitContainer}>
+        <ul className={style.fruitList}>
+          {fruits.map(
+            (fruit: {
+              id: string;
+              name: string;
+              price: number;
+              description: string;
+              image: string;
+            }) => (
+              <li className={style.fruitItem} key={fruit.id}>
+                {fruit.image && (
+                  <img
+                    className={style.fruitImage}
+                    src={fruit.image}
+                    alt={fruit.name}
+                  />
+                )}
+                <div className={style.fruitInfo}>
+                  {fruit.name}
+                  <br />
+                  Preço: R${fruit.price.toFixed(2)}
+                  {fruit.description && (
+                    <p className={style.fruitDescription}>
+                      {fruit.description}
+                    </p>
+                  )}
+                  <div className={style.buttonContainer}>
+                    <button className={style.addButton}>Comprar</button>
+                  </div>
                 </div>
-                </li>
-              ))}
-          </ul>
-        </div>
-     
-
-      
+              </li>
+            ),
+          )}
+        </ul>
+      </div>
     </div>
   );
 }
